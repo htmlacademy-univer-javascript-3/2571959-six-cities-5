d32a4065a41ffe63188c81a3ebc75e16f261f79b
Mananaraz <ivanovgleba@mail.ru>
2024-10-01 23:38:14 +0500

    Decompose main page and add empty main page

---
+++ src/components/places/places-empty.tsx
@@ -0,0 +1,20 @@
+﻿interface PlacesEmptyProps {
+  selectedCity: string;
+}
+
+export function PlacesEmpty({ selectedCity }: PlacesEmptyProps) {
+  return (
+    <>
+      <section className="cities__no-places">
+        <div className="cities__status-wrapper tabs__content">
+          <b className="cities__status">No places to stay available</b>
+          <p className="cities__status-description">
+            We could not find any property available at the moment in{' '}
+            {selectedCity}
+          </p>
+        </div>
+      </section>
+      <div className="cities__right-section" />
+    </>
+  );
+}
---
+++ src/components/tabs/tabs.tsx
@@ -0,0 +1,37 @@
+﻿import cn from 'classnames';
+
+interface TabsProps {
+  selectedCity: string;
+}
+
+export function Tabs({ selectedCity }: TabsProps) {
+  const locations = [
+    'Paris',
+    'Cologne',
+    'Brussels',
+    'Amsterdam',
+    'Hamburg',
+    'Dusseldorf',
+  ];
+
+  return (
+    <div className="tabs">
+      <section className={cn('locations', 'container')}>
+        <ul className={cn('locations__list', 'tabs__list')}>
+          {locations.map((city) => (
+            <li key={city} className="locations__item">
+              <a
+                className={cn('locations__item-link', 'tabs__item', {
+                  'tabs__item--active': city === selectedCity,
+                })}
+                href="#"
+              >
+                <span>{city}</span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+}
