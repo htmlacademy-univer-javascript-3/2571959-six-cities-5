7a7648183753c35642ec326951bb879999389264
Mananaraz <ivanovgleba@mail.ru>
2024-10-09 22:59:11 +0500

    Add routing

---
+++ src/pages/not-found/not-found-page.tsx
@@ -0,0 +1,32 @@
+﻿export function NotFoundPage() {
+  return (
+    <div className="page page--gray page--not-found">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <a className="header__logo-link" href="main.html">
+                <img
+                  className="header__logo"
+                  src="img/logo.svg"
+                  alt="6 cities logo"
+                  width={81}
+                  height={41}
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main className="page__main page__main--not-found">
+        <div className="page__not-found-container container">
+          <section className="not-found">
+            <h1 className="not-found__title">404 Not Found</h1>
+            <p className="not-found__message">Sorry, the page you are looking for does not exist.</p>
+            <a className="not-found__link" href="main.html">Go to main page</a>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
+}
---
+++ src/types/auth-status.ts
@@ -0,0 +1,5 @@
+﻿export enum AuthStatus {
+  AUTH,
+  NO_AUTH,
+  UNKNOWN,
+}
