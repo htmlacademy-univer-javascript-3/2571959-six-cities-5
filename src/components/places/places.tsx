c82252604b9da1c0ed3a66d2506636f1f5aea5d9
Mananaraz <ivanovgleba@mail.ru>
2024-11-01 23:43:45 +0500

    Add sorting order

--- src/components/places/places.tsx
+++ src/components/places/places.tsx
@@ -3,12 +3,16 @@ import { PlaceCardCities } from '../card/place-card';
 import { useState } from 'react';
 import { PlacesProps } from './types';
 import { Map } from '../map/map';
+import { sortingOrder, sorting } from '../../utils/constants';
+import { SortingOrder } from '../sorting-order/sorting-order';
+
 
 interface CityPlacesProps extends PlacesProps {
   selectedCity: string;
 }
 
 export function Places({ selectedCity, offers }: CityPlacesProps) {
+  const [order, setOrder] = useState<sortingOrder>('Price: low to high');
   const [activeCard, setActiveCard] = useState<string | null>(null);
   const points = offers.map((x) => ({
     name: x.id,
@@ -18,50 +22,21 @@ export function Places({ selectedCity, offers }: CityPlacesProps) {
     <>
       <section className={cn('cities__places', 'places')}>
         <h2 className="visually-hidden">Places</h2>
-        <b className="places__found">{offers.length} places to stay in {selectedCity}</b>
-        <form className="places__sorting" action="#" method="get">
-          <span className="places__sorting-caption">Sort by</span>
-          <span className="places__sorting-type" tabIndex={0}>
-            Popular
-            <svg className="places__sorting-arrow" width={7} height={4}>
-              <use xlinkHref="#icon-arrow-select" />
-            </svg>
-          </span>
-          <ul
-            className={cn(
-              'places__options',
-              'places__options--custom',
-              'places__options--opened'
-            )}
-          >
-            <li
-              className={cn('places__option', 'places__option--active')}
-              tabIndex={0}
-            >
-              Popular
-            </li>
-            <li className="places__option" tabIndex={0}>
-              Price: low to high
-            </li>
-            <li className="places__option" tabIndex={0}>
-              Price: high to low
-            </li>
-            <li className="places__option" tabIndex={0}>
-              Top rated first
-            </li>
-          </ul>
-        </form>
+        <b className="places__found">
+          {offers.length} places to stay in {selectedCity}
+        </b>
+        <SortingOrder order={order} onChangeOrder={setOrder} />
         <div
           className={cn('cities__places-list', 'places__list', 'tabs__content')}
         >
-          {offers.map((x) => (
+          {offers.sort(sorting[order]).map((x) => (
             <PlaceCardCities key={x.id} offer={x} onHover={setActiveCard} />
           ))}
         </div>
       </section>
       <div className="cities__right-section">
         <Map
-          city={{...points[0], location: {...points[0].location, zoom: 12}}}
+          city={{ ...points[0], location: { ...points[0].location, zoom: 12 } }}
           cities={points}
           selectedCity={points.find((x) => x.name === activeCard)}
           className="cities__map map"
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,20 @@
-﻿
+﻿import { OfferCardData } from '../types/offer';
+
+const sortingOrders = {
+  'Price: low to high': 1,
+  'Price: high to low': 2,
+  'Top rated first': 3,
+};
+
+export type sortingOrder = keyof typeof sortingOrders;
+
+export const sorting: Record<
+  sortingOrder,
+  (a: OfferCardData, b: OfferCardData) => number
+> = {
+  'Price: low to high': (a, b) => a.price - b.price,
+  'Price: high to low': (a, b) => b.price - a.price,
+  'Top rated first': (a, b) => b.rating - a.rating,
+};
+
+export const sortingKeys = Object.keys(sortingOrders);
